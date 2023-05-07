100b17b49a6bca82468a771a4c38eaad2543061f
Eduardo <edwardsendrea1@gmail.com>
2023-05-08 01:43:00 +0300

    folders/index/reduxstate/

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./homePage/Home";
+import Login from "./loginPage/Login";
+import Profile from "./profilePage/Profile";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/profile/:userId" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
