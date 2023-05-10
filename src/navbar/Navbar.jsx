738bc1f85a285b7a2f2657427c1fb3cc2006d46c
Eduardo <edwardsendrea1@gmail.com>
2023-05-11 02:03:25 +0300

    updated nav

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -1,16 +1,17 @@
-import SearchBar from "./SearchBar";
 import "./Navbar.css";
 import { useDispatch, useSelector } from "react-redux";
 import { setLogout } from "../state";
 import { useNavigate } from "react-router-dom";
 import LogoutIcon from "@mui/icons-material/Logout";
+import { IconButton } from "@mui/material";
+import { Search } from "@mui/icons-material";
 
 const Navbar = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const user = useSelector((state) => state.user);
   // const fullName = `${user.firstName} ${user.lastName}`;
-  const fullName = "Baby Yoda";
+  const fullName = "Edward Lesan";
 
   return (
     <nav>
@@ -18,11 +19,16 @@ const Navbar = () => {
         Socially.
       </div>
       <div className="nav_search">
-        <SearchBar />
+        <Search sx={{ color: "#2c5784", cursor: "pointer" }} />
+        <input placeholder="Type to search..." />
       </div>
       <div className="nav_auth">
-        <h5 className="nav_fullname">{fullName}</h5>
-        <LogoutIcon />
+        <h5 className="nav_fullname" onClick={() => navigate(`/profile/`)}>
+          {fullName}
+        </h5>
+        <IconButton onClick={() => dispatch(setLogout())}>
+          <LogoutIcon sx={{ color: "#2c5784", cursor: "pointer" }} />
+        </IconButton>
       </div>
     </nav>
   );
