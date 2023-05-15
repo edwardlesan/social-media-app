d1dcaf64db468cfd58589ae07585fe7e1ee96d85
Eduardo <edwardsendrea1@gmail.com>
2023-05-15 23:51:13 +0300

    create new post widget

---
+++ src/components/NewPost.jsx
@@ -0,0 +1,133 @@
+import "./NewPost.css";
+import {
+  BorderColorOutlined,
+  AddPhotoAlternateOutlined,
+  EditOutlined,
+  DeleteOutlined,
+} from "@mui/icons-material";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setPosts } from "../state";
+import Dropzone from "react-dropzone";
+import { IconButton, Divider } from "@mui/material";
+
+const NewPost = ({ picturePath }) => {
+  const dispatch = useDispatch();
+  const [isImage, setIsImage] = useState(false);
+  const [image, setImage] = useState(null);
+  const [post, setPost] = useState("");
+  const { _id } = useSelector((state) => state.user);
+  const token = useSelector((state) => state.token);
+
+  const handleNewPost = async () => {
+    const formData = new FormData();
+    formData.append("userId", _id);
+    formData.append("description", post);
+    if (image) {
+      formData.append("picture", image);
+      formData.append("picturePath", image.name);
+    }
+
+    const res = await fetch(`http://localhost:3001/posts`, {
+      method: "POST",
+      headers: { Authorization: `Bearer ${token}` },
+      body: formData,
+    });
+    const posts = await res.json();
+    dispatch(setPosts({ posts }));
+    setImage(null);
+    setPost("");
+  };
+
+  return (
+    <div className="new_post_card">
+      <div className="new_post_content">
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            gap: "10px",
+            padding: "0 10px",
+          }}
+        >
+          <BorderColorOutlined sx={{ color: "#2c5784" }} />
+          <h5 style={{ color: "gray" }}>Create Post</h5>
+        </div>
+        <input
+          placeholder="What's on your mind..."
+          onChange={(e) => setPost(e.target.value)}
+          value={post}
+        />
+      </div>
+
+      {isImage && (
+        <div className="new_post_image_box">
+          <Dropzone
+            acceptedFiles=".jpg,.jpeg,.png"
+            multiple={false}
+            onDrop={(acceptedFiles) => setImage(acceptedFiles[0])}
+          >
+            {({ getRootProps, getInputProps }) => (
+              <div className="new_post_image_drop" {...getRootProps()}>
+                <input {...getInputProps()} />
+                {!image ? (
+                  <p>Add Image Here</p>
+                ) : (
+                  <div
+                    style={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                      alignItems: "center",
+                    }}
+                  >
+                    <h6>{image.name}</h6>
+                    <EditOutlined
+                      sx={{ color: "#2c5784", cursor: "pointer" }}
+                    />
+                  </div>
+                )}
+
+                {image && (
+                  <IconButton
+                    onClick={() => setImage(null)}
+                    sx={{ width: "15%" }}
+                  >
+                    <DeleteOutlined
+                      sx={{ color: "#2c5784", cursor: "pointer" }}
+                    />
+                  </IconButton>
+                )}
+              </div>
+            )}
+          </Dropzone>
+        </div>
+      )}
+
+      <div className="new_post_buttons">
+        <div
+          onClick={() => setIsImage(!isImage)}
+          style={{
+            display: "flex",
+            alignItems: "center",
+            gap: "10px",
+            cursor: "pointer",
+          }}
+        >
+          <AddPhotoAlternateOutlined
+            sx={{ color: "#2c5784", cursor: "pointer" }}
+          />
+          <h5 style={{ color: "#2c5784" }}>Add Image</h5>
+        </div>
+        <button
+          className="new_post_button"
+          disabled={!post}
+          onClick={handleNewPost}
+        >
+          POST
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default NewPost;
--- src/homePage/Home.jsx
+++ src/homePage/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from "../navbar/Navbar";
 import UserComponent from "../components/UserComponent";
 import { useSelector } from "react-redux";
 import "./Home.css";
+import NewPost from "../components/NewPost";
 
 const Home = () => {
   const { _id, picturePath } = useSelector((state) => state.user);
@@ -12,6 +13,17 @@ const Home = () => {
       <Navbar />
       <div className="home_container">
         <UserComponent userId={_id} picturePath={picturePath} />
+        <div className="home_feed">
+          <NewPost picturePath={picturePath} />
+        </div>
+        <div
+          style={{
+            width: "300px",
+            height: "600px",
+            backgroundColor: "#f5f5f5",
+            borderRadius: "10px",
+          }}
+        ></div>
       </div>
     </div>
   );
